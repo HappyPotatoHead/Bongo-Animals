4f4925ccefa9f218af6aa8f5f0d4462087dbff2a
HappyPotatoHead <jimmyding9719@gmail.com>
2025-07-03 21:12:46 +0800

    Update dropDownContent.js

--- src/config/dropDownContent.js
+++ src/config/dropDownContent.js
@@ -3,14 +3,14 @@ import dog_and_maths from "../images/dog_and_maths.jpg";
 
 export const SHORK_CONTENT = {
     title: "The Blue shark",
-    description: [`That's a rather boring name, isn't it?`, ``],
-    image_text: "You wish to see me?",
+    description: [`It's a blue shark`],
+    image_text: "Piano-playing Shark",
     link_image: shark_image,
 };
 
 export const DOG_CONTENT = {
     title: "The Brown Dog",
     description: [`A mathematical wizard`],
-    image_text: "What a nerd",
+    image_text: "I want to go home",
     link_image: dog_and_maths,
 };
